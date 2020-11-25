eec12d32e3170e81a9b5858ad080f65e977c8789
Uyeye03 <clarissaangelia.s@gmail.com>
2020-11-25 23:11:39 +0800

    X Symbol done

--- js/script.js
+++ js/script.js
@@ -132,3 +132,78 @@ function Dia()
 
     document.getElementById("dispbox").innerHTML = disp; 
 }
+function X()
+{
+    var a = document.getElementById("builder").value;
+    var b = document.getElementById("layer").value;
+    var disp = '';
+    var i, j;
+
+    if (b % 2 != 0) {//bagian atas ganjil
+        for ( i = 0; i < b / 2 - 1; i++) {
+            if (i == b / 2) {
+                for ( j = 0; j < b / 2 + 1; j++) {
+                    disp += '\xa0';
+                }
+                disp += a + '<br>';
+                
+            }else {
+                for ( j = -2; j < i; j++) {
+                    disp += '\xa0';
+                }
+                disp += a;
+                for ( j = 2; j < b - i*2; j++) {
+                    disp += '\xa0';
+                }
+                disp += a + '<br>';
+            }
+        }
+    }
+    if(b % 2 != 0){
+        for(i = 0; i < b/2 + 1; i++){
+            disp += '\xa0';
+        }
+        disp += a + '<br>';
+    }
+    //bagian atas genap
+    if (b % 2 == 0){
+        for ( i = 0; i < b/2; i++){
+            for ( j = -1; j < i; j++) {
+                disp += '\xa0';
+            }
+            disp += a;
+            for ( j = 1; j < b - i * 2; j++) {
+                disp += '\xa0';
+            }
+            disp += a + '<br>';
+        }
+    }
+    //bagian bawah genap
+    if(b % 2 == 0){
+        for ( i = 0; i < b/2; i++){
+            for ( j = b/2 - i ; j > 0; j--){
+                disp += '\xa0';
+            }
+            disp += a;
+            for ( j = 0; j < i*2+1; j++){
+                disp += '\xa0';
+            }
+            disp += a + '<br>';
+        }
+    }else {//bagian bawah ganjil
+        for ( i = 0; i < b/2 - 1; i++){
+            for ( j = b/2 - i ; j > 0; j--){
+                disp += '\xa0';
+            }
+            disp += a;
+            for ( j = 0; j < i*2+1; j++){
+                disp += '\xa0';
+            }
+            disp += a + '<br>';
+        }
+    }
+    
+    
+    
+    document.getElementById("dispbox").innerHTML = disp; 
+}
